d82c016e0b8ac216c2e561cef6caa9bc3545b92b
Mykhailo Babiuk <master7dok@gmail.com>
2023-02-19 10:14:43 +0100

    added 3 Autowired for all classes

--- src/main/java/com/mbdev/MusicPlayer.java
+++ src/main/java/com/mbdev/MusicPlayer.java
@@ -1,15 +1,23 @@
 package com.mbdev;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
     private ClassicMusic classicMusic;
+    private RapMusic rapMusic;
+    private RockMusic rockMusic;
 
-    public MusicPlayer(ClassicMusic classicMusic){
+    @Autowired
+    public MusicPlayer(ClassicMusic classicMusic, RapMusic rapMusic, RockMusic rockMusic) {
         this.classicMusic = classicMusic;
+        this.rapMusic = rapMusic;
+        this.rockMusic = rockMusic;
     }
     public void playMusic(){
         System.out.println("Playing: " + classicMusic.getSong());
+        System.out.println("Playing: " + rapMusic.getSong());
+        System.out.println("Playing: " + rockMusic.getSong());
     }
 }
